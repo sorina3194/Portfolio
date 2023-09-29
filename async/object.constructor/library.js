47ffca2c16e5dc13da3744fbf2b488f3967d4295
sorina tudor <sorinatudor94@gmail.com>
2023-09-29 17:34:30 +0200

    my library remove button and read/not read button added

--- async/object.constructor/library.js
+++ async/object.constructor/library.js
@@ -4,17 +4,18 @@ const bookForm = document.getElementById("bookForm");
 const bookInputForm = document.getElementById("bookInputForm");
 const booksContainer = document.querySelector('#bookList');
 
+// book prototype
 function Book(title,author,pages,read, image) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.image = image;
   this.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} `
   }
 }
 
+// the 3 seeded books will be displayed when page is loaded
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
 myLibrary.push(book1)
 const book2 = new Book("The Adventures of Huckleberry Finn", "Mark Twain",223, true)
@@ -22,20 +23,17 @@ myLibrary.push(book2)
 const book3 = new Book("Harry Potter and the sorcerer's stone", "J.K.Rowling",300, true)
 myLibrary.push(book3)
 
-console.log(booksContainer)
+// console.log(booksContainer)
 
+// iterate over the array and call the function that creates the html elements that hosts each book
 myLibrary.forEach((book) => {
   addBookToPage(book)
 });
 
 function addBookToPage(book) {
   const bookCard = document.createElement('div');
-  bookCard.className = "card"
+  bookCard.className = "cardBook"
   booksContainer.appendChild(bookCard)
-
-  const image = document.createElement('img');
-  image.src = ''
-  bookCard.appendChild(image);
 
   const title = document.createElement('p');
   title.innerHTML = book.title;
@@ -46,17 +44,51 @@ function addBookToPage(book) {
   bookCard.appendChild(author);
 
   const pages = document.createElement('p');
-  pages.innerHTML = book.pages;
+  pages.innerHTML = `${book.pages} pages`;
   bookCard.appendChild(pages);
 
-  const read = document.createElement('p');
-  read.innerHTML = book.read ? "read" : "not read"
-  bookCard.appendChild(read);
-  console.log(book)
+  // const read = document.createElement('p');
+  // read.innerHTML = book.read ? "read" : "not read"
+  // bookCard.appendChild(read);
+  // console.log(book)
+
+  // Modify the part of your code that creates the "read" element
+  const toggleButton = document.createElement('button');
+  toggleButton.className = 'toggle-button';
+  toggleButton.textContent = book.read ? 'Read' : 'Not Read';
+  bookCard.appendChild(toggleButton);
+
+  // Add an event listener to toggle the button
+  toggleButton.addEventListener('click', () => {
+      book.read = !book.read; // Toggle the 'read' property
+
+      // Update the button text and class based on the new 'read' value
+      toggleButton.textContent = book.read ? 'Read' : 'Not Read';
+  });
+
+  // create a remove book button and the function to perform such action
+  const removeBook = document.createElement('button');
+  removeBook.innerHTML = "Remove Book"
+  bookCard.appendChild(removeBook);
+
+  removeBook.addEventListener("click", function(event) {
+    const confirmation = window.confirm('Are you sure you want to remove this book?');
+    if (confirmation) {
+      // Remove the card from the DOM
+      // bookCard.remove();
+      booksContainer.removeChild(bookCard)
+
+      // Find the index of the book in the 'myLibrary' array and remove it
+      const index = myLibrary.indexOf(book);
+      if (index !== -1) {
+          myLibrary.splice(index, 1);
+      }
+    }
+  });
 }
 
 
-function addBook() {
+function addBookToRecord() {
   showFormButton.addEventListener("click", function () {
       bookForm.style.display = "block";
   });
@@ -80,4 +112,4 @@ function addBook() {
   });
 }
 
-document.addEventListener("DOMContentLoaded",addBook);
+document.addEventListener("DOMContentLoaded",addBookToRecord);
